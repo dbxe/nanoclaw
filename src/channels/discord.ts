a7dbb940279a1f8d81d2cc39463d1b2154feaae4
Daniel Briskin <daniel.briskin@gmail.com>
2026-05-02 19:45:59 +0900

    Split long Discord replies

--- src/channels/discord.ts
+++ src/channels/discord.ts
@@ -32,6 +32,7 @@ registerChannelAdapter('discord', {
       concurrency: 'concurrent',
       botToken: env.DISCORD_BOT_TOKEN,
       extractReplyContext,
+      maxTextLength: 1900,
       supportsThreads: true,
     });
   },
